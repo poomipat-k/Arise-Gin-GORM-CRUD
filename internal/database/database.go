ecdcbb507cdd07ee10c10ae52c0279a54185ced0
poomipat-k <k.poomipat@gmail.com>
2025-07-30 15:32:20 +0700

    init project

---
+++ internal/database/database.go
@@ -0,0 +1,13 @@
+package database
+
+import (
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	// dsn := fmt.Sprintf(
+	// 	"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+	// )
+}
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+	// load config
+
+	// Init database
+
+}
