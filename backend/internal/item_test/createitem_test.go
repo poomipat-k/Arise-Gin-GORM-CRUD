465be41beae31f66d60e6c6259fea51efd7eefc5
poomipat-k <k.poomipat@gmail.com>
2025-07-31 13:47:19 +0700

    crud table test CreateItem

--- backend/internal/item_test/createitem_test.go
+++ backend/internal/item_test/createitem_test.go
@@ -15,38 +15,90 @@ import (
 	"github.com/poomipat-k/crud-arise/internal/schemas"
 )
 
+type ResponseBody struct {
+	Id     int
+	Status string
+}
+
 func TestCreateItemHandler(t *testing.T) {
 	gin.SetMode(gin.TestMode)
 
-	name := "Reading"
-	input := schemas.CreateItemSchemaInput{
-		Name: name,
-	}
-
-	inputStr, err := json.Marshal(input)
-	if err != nil {
-		log.Fatalf("error Marshal input err: %v", err.Error())
-	}
-
-	res := httptest.NewRecorder()
-	c, r := gin.CreateTestContext(res)
-	c.Request = httptest.NewRequest(http.MethodPost, "/items", bytes.NewBuffer(inputStr))
-	c.Request.Header.Set("Content-Type", "application/json")
-
-	mockItemStore := &MockItemStore{
-		CreateItemFunc: func(input schemas.CreateItemSchemaInput) (*models.Item, error) {
-			return &models.Item{
-				ID: 1,
-			}, errors.New("New Error")
+	tests := []struct {
+		testName       string
+		name           string
+		store          *MockItemStore
+		expectedStatus int
+		expectedId     int
+	}{
+		{
+			testName:       "should error when name is empty",
+			name:           "",
+			expectedStatus: http.StatusBadRequest},
+		{
+			testName:       "should error when name length is greater than 100",
+			name:           "abcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxya", // 101 chars
+			expectedStatus: http.StatusBadRequest,
+		},
+		{
+			testName:       "should error when store failed to create item",
+			name:           "Testing",
+			expectedStatus: http.StatusInternalServerError,
+			store: &MockItemStore{
+				CreateItemFunc: func(input schemas.CreateItemSchemaInput) (*models.Item, error) {
+					return nil, errors.New("something wrong")
+				},
+			},
+		},
+		{
+			testName:       "should create item successfully",
+			name:           "Testing",
+			expectedStatus: http.StatusOK,
+			store: &MockItemStore{
+				CreateItemFunc: func(input schemas.CreateItemSchemaInput) (*models.Item, error) {
+					return &models.Item{
+						Name: "Testing",
+						ID:   1,
+					}, nil
+				},
+			},
+			expectedId: 1,
 		},
 	}
-	itemHandler := item.NewItemHandler(mockItemStore)
 
-	r.POST("/items", itemHandler.CreateItem)
-	r.ServeHTTP(res, c.Request)
+	for _, tt := range tests {
+		t.Run(tt.testName, func(t *testing.T) {
+			input := schemas.CreateItemSchemaInput{
+				Name: tt.name,
+			}
 
-	if status := res.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+			inputStr, err := json.Marshal(input)
+			if err != nil {
+				log.Fatalf("error Marshal input err: %v", err)
+			}
+
+			res := httptest.NewRecorder()
+			c, r := gin.CreateTestContext(res)
+			c.Request = httptest.NewRequest(http.MethodPost, "/items", bytes.NewBuffer(inputStr))
+			c.Request.Header.Set("Content-Type", "application/json")
+
+			itemHandler := item.NewItemHandler(tt.store)
+
+			r.POST("/items", itemHandler.CreateItem)
+			r.ServeHTTP(res, c.Request)
+
+			if status := res.Code; status != tt.expectedStatus {
+				t.Errorf("handler returned wrong status code: got %v want %v", status, tt.expectedStatus)
+			}
+			if tt.expectedId > 0 {
+				var body ResponseBody
+				err := json.Unmarshal(res.Body.Bytes(), &body)
+				if err != nil {
+					t.Errorf("Error unmarshal ResponseBody err:%v", err)
+				}
+				if body.Id != tt.expectedId {
+					t.Errorf("handler returned wrong Id: got %v want %v", body.Id, tt.expectedId)
+				}
+			}
+		})
 	}
-
 }
